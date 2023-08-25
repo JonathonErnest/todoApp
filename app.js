a24730c4223067bb71f0abaf1e2968a9019d3ac1
Jonathon Ernest <miahcodes247@gmail.com>
2023-08-24 23:45:54 -0500

    made some adjustments to the task list

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ window.addEventListener('load', () => {
         const task_el = document.createElement("div");
         task_el.classList.add("task");
 
-        const task_content_el= document.createElement("div");
+        const task_content_el = document.createElement("div");
         task_content_el.classList.add("content");
 
         task_el.appendChild(task_content_el);
